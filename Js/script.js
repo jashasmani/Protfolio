1249b26d47ccde9f0a4bb4a52b3f26c25b292523
jash2504 <132358666+jash2504@users.noreply.github.com>
2023-12-07 00:09:28 +0530

    k

--- Js/script.js
+++ Js/script.js
@@ -1,19 +1,34 @@
-const sections = document.querySelectorAll('section[id]')
+document.addEventListener("DOMContentLoaded", function() {
+     // Get all the navigation links
+     const navLinks = document.querySelectorAll(".navbar1 a");
 
-const scrollActive = ()=>{
-     const scrollDown = window.scrollY
+     // Function to update the active link based on scroll position
+     function updateActiveLink() {
+         const sections = document.querySelectorAll("section");
+         let currentSectionId = "";
 
-     sections.forEach(current=>{
-          const sectionHeight = current.offsetHeight,
-                sectionTop = current.offsetTop - 58,
-                sectionId = current.getAttribute('id'),
-                sectionsClass = document.querySelector('.nav__menu a[href*=' +sectionId + ']')
+         sections.forEach(section => {
+             const rect = section.getBoundingClientRect();
+             if (rect.top <= 50 && rect.bottom >= 50) {
+                 currentSectionId = section.id;
+             }
+         });
 
-          if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-               sectionsClass.classList.add('active-link')
-          }else{
-               sectionsClass.classList.remove('active-link')
-          }
-     })
-}
-window.addEventListener('scroll',scrollActive)
+         // Remove the "active" class from all links
+         navLinks.forEach(link => {
+             link.classList.remove("active");
+         });
+
+         // Add the "active" class to the link corresponding to the current section
+         const activeLink = document.querySelector(.navbar1 a[href="#${currentSectionId}"]);
+         if (activeLink) {
+             activeLink.classList.add("active");
+         }
+     }
+
+     // Attach the updateActiveLink function to the scroll event
+     window.addEventListener("scroll", updateActiveLink);
+
+     // Call the updateActiveLink function once to set the initial state
+     updateActiveLink();
+ });
