07abdf5a2f58f95ef82ef964015e097a65649c3c
jash2504 <132358666+jash2504@users.noreply.github.com>
2023-12-07 16:54:35 +0530

    ok

--- Js/script.js
+++ Js/script.js
@@ -1,34 +1,70 @@
+
+
 document.addEventListener("DOMContentLoaded", function() {
-     // Get all the navigation links
-     const navLinks = document.querySelectorAll(".navbar1 a");
+  // Get all the section elements
+  const sections = document.querySelectorAll("section");
 
-     // Function to update the active link based on scroll position
-     function updateActiveLink() {
-         const sections = document.querySelectorAll("section");
-         let currentSectionId = "";
+  // Get all the navigation links
+  const navLinks = document.querySelectorAll(".navbar1 a");
 
-         sections.forEach(section => {
-             const rect = section.getBoundingClientRect();
-             if (rect.top <= 50 && rect.bottom >= 50) {
-                 currentSectionId = section.id;
-             }
-         });
+  // Function to update the active link based on the scroll position
+  function updateActiveLink() {
+    let currentScroll = window.scrollY;
 
-         // Remove the "active" class from all links
-         navLinks.forEach(link => {
-             link.classList.remove("active");
-         });
+    sections.forEach((section, index) => {
+      const sectionTop = section.offsetTop - 50; // Adjusted for better accuracy
+      const sectionBottom = sectionTop + section.offsetHeight;
 
-         // Add the "active" class to the link corresponding to the current section
-         const activeLink = document.querySelector(.navbar1 a[href="#${currentSectionId}"]);
-         if (activeLink) {
-             activeLink.classList.add("active");
-         }
-     }
+      if (currentScroll >= sectionTop && currentScroll < sectionBottom) {
+        navLinks.forEach(link => link.classList.remove("active"));
+        navLinks[index].classList.add("active");
+      }
+    });
+  }
 
-     // Attach the updateActiveLink function to the scroll event
-     window.addEventListener("scroll", updateActiveLink);
+  // Attach the scroll event listener
+  window.addEventListener("scroll", updateActiveLink);
 
-     // Call the updateActiveLink function once to set the initial state
-     updateActiveLink();
- });
+  // Attach the click event listener to smooth scroll when a navigation link is clicked
+  navLinks.forEach(link => {
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute("href").substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      window.scrollTo({
+        top: targetSection.offsetTop - 48, // Adjusted for better accuracy
+        behavior: "smooth"
+      });
+    });
+  });
+});
+
+
+
+//  
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get all elements with the class 'load-animation'
+    const elements = document.querySelectorAll('.load-animation');
+  
+    // Function to add the 'animate' class when an element comes into view
+    function animateElements() {
+      elements.forEach(element => {
+        const elementTop = element.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+  
+        if (elementTop < windowHeight) {
+          element.classList.add('animate');
+        }
+      });
+    }
+  
+    // Attach the scroll event listener
+    window.addEventListener('scroll', animateElements);
+  
+    // Initial animation check
+    animateElements();
+  });
