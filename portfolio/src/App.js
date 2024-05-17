4ab1912d560a168c4bcec90422849b01024d054f
jash2504 <asmanijash51@gmail.com>
2024-05-17 15:05:24 +0530

    Commit 1

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { MainNavbar } from './Component/Navbar/MainNavbar';
+
+function App() {
+  return (
+    <div>
+      <MainNavbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/Component/Navbar/MainNavbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './MainNavbar.css'
+
+export const MainNavbar = () => {
+  return (
+    <div>
+        <nav className='navbar'>
+            
+            <span>Jash Asmani</span>
+        </nav>
+    </div>
+  )
+}
