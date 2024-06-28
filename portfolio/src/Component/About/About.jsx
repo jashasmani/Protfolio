6a6d68c0bb7ce1b50182e41282b3ccd053aab7a9
jash asmani <asmanijash51@gmail.com>
2024-06-28 22:55:18 +0530

    Change

--- portfolio/src/Component/About/About.jsx
+++ portfolio/src/Component/About/About.jsx
@@ -17,20 +17,19 @@ const About = () => {
           technologies. I build robust back-end services with
           <span className="highlight"> Spring Boot</span> and
           <span className="highlight"> Node.js</span>, and create dynamic
-          front-end applications using <span className="highlight">React.js</span>.
-          My cloud expertise includes deploying and managing applications on
+          front-end applications using{" "}
+          <span className="highlight">React.js</span>. My cloud expertise
+          includes deploying and managing applications on
           <span className="highlight"> AWS</span>. Proficient in
           <span className="highlight"> Python</span>,
           <span className="highlight"> Java</span>, and
-          <span className="highlight"> JavaScript</span>, I also excel in database
-          management with <span className="highlight">PostgreSQL</span>,
-          <span className="highlight"> MySQL</span>, and
+          <span className="highlight"> JavaScript</span>, I also excel in
+          database management with <span className="highlight">PostgreSQL</span>
+          ,<span className="highlight"> MySQL</span>, and
           <span className="highlight"> MongoDB</span>. Additionally, I have
           experience in mobile development with
           <span className="highlight"> Android</span> and
-          <span className="highlight"> Flutter</span>, and I specialize in
-          <span className="highlight"> machine learning</span> and
-          <span className="highlight"> data analysis</span>. My diverse skill set
+          <span className="highlight"> Flutter.</span> My diverse skill set
           enables me to tackle complex projects and deliver efficient,
           high-quality solutions.
         </p>
